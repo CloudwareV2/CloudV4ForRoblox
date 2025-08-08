8bec41502ac6d499a03607548cbda6990bb88f60
sstvskids <126665568+sstvskids@users.noreply.github.com>
2025-08-08 16:56:12 +1000

    Update 6872274481.lua

--- games/6872274481.lua
+++ games/6872274481.lua
@@ -7793,7 +7793,7 @@ run(function()
 		Name = 'HitFix',
 		Function = function(callback)
 			debug.setconstant(bedwars.SwordController.swingSwordAtMouse, 23, callback and 'raycast' or 'Raycast')
-			debug.setupvalue(bedwars.SwordController.swingSwordAtMouse, 4, callback and bedwars.QueryUtil or workspace)
+			debug.setupvalue(bedwars.SwordController.swingSwordAtMouse, 4, workspace)
 		end,
 		Tooltip = 'Changes the raycast function to the correct one'
 	})
