65531c64a5894e59a36c8019b29ce5be5c66fd3d
sstvskids <staavstudios@gmail.com>
2025-08-26 20:50:53 +1000

    Update 6872274481.lua

--- games/6872274481.lua
+++ games/6872274481.lua
@@ -9676,10 +9676,7 @@ run(function()
 		Function = function(callback)
 			if callback then
 				repeat
-					if getItem('jade_hammer') then
-						bedwars.AbilityController:useAbility('jade_hammer_jump')
-					end
-
+					bedwars.AbilityController:useAbility('jade_hammer_jump')
 					task.wait()
 				until not Disabler.Enabled
 			end
